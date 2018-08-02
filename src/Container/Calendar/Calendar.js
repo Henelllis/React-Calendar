f72f2b06cde675c3c57c238ddc32824dc707cf39
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2018-08-02 16:35:59 -0400

    Intial mock up of Calendar

--- src/Container/Calendar/Calendar.js
+++ src/Container/Calendar/Calendar.js
@@ -16,6 +16,60 @@ class Calendar extends Component {
             <span className ={styles.monthHeader}> AUGUST </span>
             <table className={styles.table}>
                 <CalenderHeader/>
+            <tr>
+                <td  className={styles.unUsedDay}> </td>
+                <td  className={styles.unUsedDay}> </td>
+                <td className={styles.calendarDay}> 
+                    <div className={styles.dayNumber}>1</div>
+                    <button className={styles.exitButton}>  + </button>
+                    <ul className={styles.reminderList}>
+                        <li>12:15 AM</li>
+                        <li>1:15 AM</li>
+                        <li>2:15 AM</li>
+                        <li>3:15 AM</li>
+                    </ul>
+                 </td>
+                 <td className={styles.calendarDay}> 
+                    <div className={styles.dayNumber}>2</div>
+                    <button className={styles.exitButton}>  + </button>
+                    <ul className={styles.reminderList}>
+                        <li>12:15 AM</li>
+                        <li>1:15 AM</li>
+                        <li>2:15 AM</li>
+                        <li>3:15 AM</li>
+                    </ul>
+                 </td>
+                 <td className={styles.calendarDay}> 
+                    <div className={styles.dayNumber}>3</div>
+                    <button className={styles.exitButton}>  + </button>
+                    <ul className={styles.reminderList}>
+                        <li>12:15 AM</li>
+                        <li>1:15 AM</li>
+                        <li>2:15 AM</li>
+                        <li>3:15 AM</li>
+                    </ul>
+                 </td>
+                 <td className={styles.calendarDay}> 
+                    <div className={styles.dayNumber}>4</div>
+                    <button className={styles.exitButton}>  + </button>
+                    <ul className={styles.reminderList}>
+                        <li>12:15 AM</li>
+                        <li>1:15 AM</li>
+                        <li>2:15 AM</li>
+                        <li>3:15 AM</li>
+                    </ul>
+                 </td>
+                 <td className={styles.calendarDay}> 
+                    <div className={styles.dayNumber}>6</div>
+                    <button className={styles.exitButton}>  + </button>
+                    <ul className={styles.reminderList}>
+                        <li>12:15 AM</li>
+                        <li>1:15 AM</li>
+                        <li>2:15 AM</li>
+                        <li>3:15 AM</li>
+                    </ul>
+                 </td>
+            </tr>
             </table>
             </div>
         )
