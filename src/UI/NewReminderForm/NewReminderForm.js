3c1ac95e696056d5640cbb7d275d8e369d540ddf
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2018-08-03 20:07:22 -0400

    State manipulation in New Reminder Form

--- src/UI/NewReminderForm/NewReminderForm.js
+++ src/UI/NewReminderForm/NewReminderForm.js
@@ -6,15 +6,43 @@ import Backdrop from  '../Backdrop/Backdrop';
 
 class NewReminderForm extends Component{ 
 
+    state = {
+        time: null,
+        reminderText: null
+    }
+
     shouldComponentUpdate(nextProps, nextState) {
         return (nextProps.show !== this.props.show || nextProps.children !== this.props.children)
 
     }
 
+    onReminderTextChange = (event) => {
+        console.log(event.target.value);
+         
+        this.setState({reminderText: event.target.value})
+        console.log('[REMAINDER TEXT] : ', event.target.value);
+    }
+
+    onSubmitHandler = (event ) => {
+        event.preventDefault();
+        event.target.reset();
+        console.log("YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        this.setState({reminderText: ""})
+        this.setState({reminderText: null})
+        //Invoke a function to set parent component to set show to false, at the end of course 
+        this.props.closeAddReminder();
+    }
+
 
 
     render() {
+        let index = 0;
+        if( this.props.workingIndex){
+            index = this.props.workingIndex - 1;
+        }
+        console.log('[REMAINDER TEXT] : ', this.state.reminderText);
         return(
+           
             <Fragment>
                 {this.props.children}
                 <Backdrop show={this.props.show} clicked={this.props.reminderClosed}/>
@@ -25,11 +53,17 @@ class NewReminderForm extends Component{
                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
                         opacity: this.props.show ? '1' : '0',
                     }}>
-                    <p>{this.props.workingIndex}</p>
-                    <form>
-                        <input placeholder="ENTER TIME"/>
-                        <input placeholder="ENTER MESSAGE"/>
-                        <button> ADD REMINDER </button>
+                    <p>{this.props.days[index].date}</p>
+                    
+                    <form className={styles.formStyle}  onSubmit={this.onSubmitHandler}>
+                        <input  className={styles.formInput}type="time" placeholder="ENTER TIME"/>
+                        {/* TODO: ADD VALIDATION OF IF CHAR > 0 AND <= 30 DO NOT NEED JSX */}
+                        <input className={styles.formInput} type="text" 
+                                                            placeholder="ENTER MESSAGE"
+                                                            value={this.state.reminderText}
+                                                            onChange={this.onReminderTextChange}/>
+                        <button 
+                           > ADD REMINDER </button>
                     </form>
                 </div>
             </Fragment>
